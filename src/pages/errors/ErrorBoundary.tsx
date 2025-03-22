647a6252b4273c0335a6b3433c256db3ce7e519a
df-van <df.vanvan@gmail.com>
2025-03-22 16:08:47 +0900

    feat : not found, transfer failed 페이지 스타일링

--- src/pages/errors/ErrorBoundary.tsx
+++ src/pages/errors/ErrorBoundary.tsx
@@ -1,18 +1,37 @@
-import {useNavigate, useRouteError} from "react-router-dom";
-import {PATH} from "../../config";
+import { useNavigate, useRouteError } from "react-router-dom";
+import { PATH } from "@/config";
+import TopAreaWrapper from "@/components/common/TopAreaWrapper";
+import Image from "@/assets/images/img_failed.png";
+import ImageX2 from "@/assets/images/img_failed_x2.png";
+import BottomAreaWrapper from "@/components/common/BottomAreaWrapper";
+import ConfirmButton from "@/components/common/ConfirmButton";
 
 export default function ErrorBoundary() {
   const error = useRouteError();
   const navigate = useNavigate();
-  const onClick = () => {
+  const handleConfirm = () => {
     navigate(PATH.HOME);
-  }
+  };
   return (
     <>
-      <h1>예상치 못한 오류 발생</h1>
-      <p>문제가 발생했습니다. 다시 시도해 주세요.</p>
-      {error && <pre>{JSON.stringify(error, null, 2)}</pre>}
-      <button onClick={onClick}>홈으로 돌아가기</button>
+      <TopAreaWrapper
+        images={[Image, ImageX2]}
+        alt="송금 실패"
+        className="flex-1"
+      >
+        <p className="text-xl font-semibold">예상치 못한 오류 발생</p>
+        <p className="text-[#FF3C3C] whitespace-pre-line text-center">
+          문제가 발생했습니다. 다시 시도해 주세요.
+        </p>
+        <p className="text-[#FF3C3C] whitespace-pre-line text-center">
+          Create new scratch file from selection
+        </p>
+      </TopAreaWrapper>
+      <BottomAreaWrapper>
+        <ConfirmButton variant="danger" onClick={handleConfirm}>
+          홈으로 돌아가기
+        </ConfirmButton>
+      </BottomAreaWrapper>
     </>
-  )
+  );
 }
--- src/pages/errors/NotFound.tsx
+++ src/pages/errors/NotFound.tsx
@@ -1,16 +1,33 @@
-import {useNavigate} from "react-router-dom";
-import {PATH} from "../../config";
+import { useNavigate } from "react-router-dom";
+import { PATH } from "@/config";
+import ImageX2 from "@/assets/images/img_failed_x2.png";
+import TopAreaWrapper from "@/components/common/TopAreaWrapper";
+import Image from "@/assets/images/img_failed.png";
+import ConfirmButton from "@/components/common/ConfirmButton";
+import BottomAreaWrapper from "@/components/common/BottomAreaWrapper";
 
-export default function NotFound(){
+export default function NotFound() {
   const navigate = useNavigate();
-  const onClick = ()=>{
+  const handleConfirm = () => {
     navigate(PATH.HOME);
-  }
+  };
   return (
-    <div className="not-found">
-      <h2>페이지를 찾을 수 없습니다.</h2>
-      <p>잘못된 경로로 접근하셨습니다.</p>
-      <button onClick={onClick}>홈으로</button>
-    </div>
+    <>
+      <TopAreaWrapper
+        images={[Image, ImageX2]}
+        alt="송금 실패"
+        className="flex-1"
+      >
+        <p className="text-xl font-semibold">페이지를 찾을 수 없습니다</p>
+        <p className="text-[#FF3C3C] whitespace-pre-line text-center">
+          잘못된 경로로 접근하셨습니다.
+        </p>
+      </TopAreaWrapper>
+      <BottomAreaWrapper>
+        <ConfirmButton variant="danger" onClick={handleConfirm}>
+          홈으로 돌아가기
+        </ConfirmButton>
+      </BottomAreaWrapper>
+    </>
   );
 }
